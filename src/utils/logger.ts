fdf251836eb4a43d865fdc6435c1cddbbc428f69
wolf-yuan <wolf@wolf-yuan.dev>
2024-01-22 16:34:15 +0800

    feat(utils/logger.ts): add logger

---
+++ src/utils/logger.ts
@@ -0,0 +1,60 @@
+import chalk from "chalk";
+
+function getDateString(): string {
+  const now = new Date();
+  const pad = (num: number) => num.toString().padStart(2, "0");
+
+  const month = pad(now.getMonth() + 1);
+  const date = pad(now.getDate());
+  const hours = pad(now.getHours());
+  const minutes = pad(now.getMinutes());
+  const seconds = pad(now.getSeconds());
+
+  return `${chalk.dim(`${month}/${date}`)} ${chalk.cyan(
+    `${hours}:${minutes}:${seconds}`
+  )}`;
+}
+
+function formatString(input: string) {
+  return input + " ".repeat(Math.max(0, 20 - input.length));
+}
+
+export function info(message: string, sender: string): void {
+  console.log(
+    `${getDateString()} ${formatString(chalk.green(sender))}${chalk.blue(
+      "info"
+    )} ${message}`
+  );
+}
+
+export function warn(message: string, sender: string): void {
+  console.log(
+    `${getDateString()} ${formatString(
+      chalk.green(sender)
+    )}${chalk.yellowBright("warn")} ${message}`
+  );
+}
+
+export function error(message: string, sender: string, error?: unknown): void {
+  console.error(
+    `${getDateString()} ${formatString(chalk.green(sender))}${chalk.red(
+      "error"
+    )} ${message}`
+  );
+
+  if (error instanceof Error) {
+    const prefixed = (error.stack ?? "Stacktrace not available")
+      .split("\n")
+      .map(
+        (line) =>
+          `${getDateString()} ${formatString(chalk.yellow("debug"))}${line}`
+      );
+    console.error(prefixed.join("\n"));
+  }
+}
+
+export default {
+  info,
+  warn,
+  error,
+};
